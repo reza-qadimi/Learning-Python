8e0690fe69bd556594d8f3e46bc210be36827ad6
reza-qadimi <rezaqadimi.ir@gmail.com>
2023-03-09 22:35:36 +0330

    make some changes.

--- src/Learning_02/DataTypes/string.py
+++ src/Learning_02/DataTypes/string.py
@@ -281,3 +281,213 @@
 
 #print(messageParts)
 #-------------------------
+#-------------------------
+#-------------------------
+
+#-------------------------
+#-------------------------
+#-------------------------
+#firstPart = "Hello,"
+#secondPart = "World!"
+
+#message = firstPart + secondPart;
+
+#print(message)
+#-------------------------
+
+#-------------------------
+#firstPart = "Hello,"
+#secondPart = "World!"
+
+#message = firstPart + " " + secondPart;
+
+#print(message)
+#-------------------------
+#-------------------------
+#-------------------------
+
+#-------------------------
+#-------------------------
+#-------------------------
+##age = 27 # Runtime Error
+#age = '27'
+#message = "Hello, World! I'm Reza Qadimi and I'm "
+
+#result = message + age
+
+#print(result)
+#-------------------------
+
+#-------------------------
+#age = 27
+#name = 'Reza Qadimi'
+#message = "Hello, World! I'm {} and I'm {}"
+
+#result = message.format(name, age)
+
+#print(result)
+#-------------------------
+
+#-------------------------
+#age = 27
+#name = 'Reza Qadimi'
+#message = "Hello, World! I'm {0} and I'm {1}"
+
+#result = message.format(name, age)
+
+#print(result)
+#-------------------------
+
+#-------------------------
+#age = 27
+#name = 'Reza Qadimi'
+#message = "Hello, World! I'm {1} and I'm {0}"
+
+#result = message.format(name, age)
+
+#print(result)
+#-------------------------
+
+#-------------------------
+#age = 27
+#name = 'Reza Qadimi'
+#message = "Hello, World! I'm {1} and I'm {2}"
+
+#result = message.format(name, age)
+# #Replacement index 2 out of range for positional args tuple
+
+#print(result)
+#-------------------------
+#-------------------------
+#-------------------------
+
+#-------------------------
+#-------------------------
+#-------------------------
+
+##\'		Single Quote
+
+#value = 'Single Quote: \' Sample';
+
+#print(value);
+
+#-------------------------
+
+##\\		Backslash
+
+#value = 'Backslash: \\ Sample';
+
+#print(value);
+
+#-------------------------
+
+##\n		New Line
+
+#value = 'New Line: \n Sample';
+
+#print(value);
+
+#-------------------------
+
+##\r		Carriage Return
+
+#value = 'Carriage Return: \r Sample';
+
+#print(value);
+
+#-------------------------
+
+##\t		Tab
+
+#value = 'Tab: \t Sample';
+
+#print(value);
+
+#-------------------------
+
+##\b		Backspace
+
+#value = 'Backspace: \b Sample';
+
+#print(value);
+
+#-------------------------
+
+##\f		Form Feed
+
+#value = 'Form Feed: \f Sample';
+
+#print(value);
+
+#-------------------------
+
+##\ooo	Octal value
+
+#value = 'Octal value: \110\145\154\154\157 Sample';
+
+#print(value);
+
+#-------------------------
+
+##\xhh	Hex value
+
+#value = '\x48\x65\x6c\x6c\x6f';
+
+#print(value);
+
+#-------------------------
+#-------------------------
+#-------------------------
+
+#-------------------------
+#-------------------------
+#-------------------------
+#https://www.w3schools.com/python/python_strings_methods.asp
+
+#center()		Returns a centered string
+#translate()	Returns a translated string
+#casefold()		Converts string into lower case
+#expandtabs()	Sets the tab size of the string
+#upper()		Converts a string into upper case
+#lower()		Converts a string into lower case
+#format()		Formats specified values in a string
+#format_map()	Formats specified values in a string
+#strip()		Returns a trimmed version of the string
+#encode()		Returns an encoded version of the string
+#lstrip()		Returns a left trim version of the string
+#capitalize()	Converts the first character to upper case
+#rstrip()		Returns a right trim version of the string
+#isidentifier()	Returns True if the string is an identifier
+#ljust()		Returns a left justified version of the string
+#rjust()		Returns a right justified version of the string
+#splitlines()	Splits the string at line breaks and returns a list
+#maketrans()	Returns a translation table to be used in translations
+#istitle()		Returns True if the string follows the rules of a title
+#isdigit()		Returns True if all characters in the string are digits
+#title()		Converts the first character of each word to upper case
+#isnumeric()	Returns True if all characters in the string are numeric
+#endswith()		Returns true if the string ends with the specified value
+#isdecimal()	Returns True if all characters in the string are decimals
+#swapcase()		Swaps cases, lower case becomes upper case and vice versa
+#isprintable()	Returns True if all characters in the string are printable
+#join()			Joins the elements of an iterable to the end of the string
+#startswith()	Returns true if the string starts with the specified value
+#rpartition()	Returns a tuple where the string is parted into three parts
+#islower()		Returns True if all characters in the string are lower case
+#isupper()		Returns True if all characters in the string are upper case
+#partition()	Returns a tuple where the string is parted into three parts
+#isspace()		Returns True if all characters in the string are whitespaces
+#isalnum()		Returns True if all characters in the string are alphanumeric
+#count()		Returns the number of times a specified value occurs in a string
+#isalpha()		Returns True if all characters in the string are in the alphabet
+#rsplit()		Splits the string at the specified separator, and returns a list
+#split()		Splits the string at the specified separator, and returns a list
+#zfill()		Fills the string with a specified number of 0 values at the beginning
+#replace()		Returns a string where a specified value is replaced with a specified value
+#find()			Searches the string for a specified value and returns the position of where it was found
+#index()		Searches the string for a specified value and returns the position of where it was found
+#rfind()		Searches the string for a specified value and returns the last position of where it was found
+#rindex()		Searches the string for a specified value and returns the last position of where it was found
+#-------------------------
+#-------------------------
+#-------------------------
