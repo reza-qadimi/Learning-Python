10376b50226025c1e4ec320da7e3114ba6d0db80
reza-qadimi <rezaqadimi.ir@gmail.com>
2023-03-09 22:49:20 +0330

    feature: number

--- src/Learning_02/DataTypes/number.py
+++ src/Learning_02/DataTypes/number.py
@@ -1,1 +1,76 @@
+#-------------------------
+#int
+#float
+#complex??
+#-------------------------
 
+#-------------------------
+#-------------------------
+#-------------------------
+#int
+#-------------------------
+
+#integerTypedValue = 1
+#print(integerTypedValue)
+
+#integerValueType = type(integerTypedValue);
+#print(integerValueType)
+
+#-------------------------
+#float
+#-------------------------
+
+#floatTypedValue = 1.2
+#print(floatTypedValue)
+
+#floatValueType = type(floatTypedValue);
+#print(floatValueType)
+
+
+#floatTypedValue = 35e3
+#print(floatTypedValue)
+
+#floatValueType = type(floatTypedValue);
+#print(floatValueType)
+
+#-------------------------
+#complex
+#-------------------------
+
+#complexTypedValue = 1j
+#print(complexTypedValue)
+
+#complexValueType = type(complexTypedValue);
+#print(complexValueType)
+
+#-------------------------
+#-------------------------
+#-------------------------
+
+#-------------------------
+#-------------------------
+#-------------------------
+#import random;
+
+#randomNumber = random.randrange(1, 10);
+
+#result = 'randrange: {}'.format(randomNumber)
+
+#print(result);
+
+
+#randomNumber = random.random();
+
+#result = 'random: {}'.format(randomNumber)
+
+#print(result);
+
+
+#randomNumber = random.randint(1, 10);
+
+#result = 'randint: {}'.format(randomNumber)
+
+#print(result);
+#-------------------------
+#-------------------------
+#-------------------------
