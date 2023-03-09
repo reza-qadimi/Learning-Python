6e24837e3bad8a2369167958376820932889fc37
reza-qadimi <rezaqadimi.ir@gmail.com>
2023-03-09 21:52:56 +0330

    feature: string.

--- src/Learning_02/DataTypes/string.py
+++ src/Learning_02/DataTypes/string.py
@@ -1,1 +1,283 @@
+#-------------------------
+#-------------------------
+#-------------------------
+#print('Hello, World!')
 
+#print("Hello, World!")
+#-------------------------
+
+#-------------------------
+#message = 'Hello, World!';
+
+#print(message);
+#-------------------------
+
+#-------------------------
+#multilineText = """
+#Hello,
+#World
+#!"""
+
+#print(multilineText)
+
+#multilineText = '''
+#Hello,
+#	World
+#		!
+#	'''
+
+#print(multilineText)
+#-------------------------
+#-------------------------
+#-------------------------
+
+#-------------------------
+#-------------------------
+#-------------------------
+#message = "Hello, World!"
+
+#firstCharacter  = message[0]
+
+#print(firstCharacter)
+#-------------------------
+
+#-------------------------
+#message = "Hello, World!"
+
+#for character in message:
+#	print(character)
+#-------------------------
+
+#-------------------------
+#message = "Hello, World!"
+
+#messageLength = len(message)
+
+#print(messageLength)
+#-------------------------
+#-------------------------
+#-------------------------
+
+#-------------------------
+#-------------------------
+#-------------------------
+#message = "Hello, World!"
+
+#foundAny1 = "hello" in message
+#foundAny2 = "Hello" in message
+
+#print(foundAny1)
+#print(foundAny2)
+#-------------------------
+
+#-------------------------
+#message = "Hello, World!"
+
+#foundAny1 = "hello" not in message
+#foundAny2 = "Hello" not in message
+
+#print(foundAny1)
+#print(foundAny2)
+#-------------------------
+
+#-------------------------
+#message = "Hello, World!"
+
+#if "hello" in message:
+#	print(True)
+
+#if "hello" not in message:
+#	print(False)
+#-------------------------
+#-------------------------
+#-------------------------
+
+#-------------------------
+#-------------------------
+#-------------------------
+#message = "Hello, World!"
+
+#print(message)
+
+#selectedPart = message[3: 5];
+
+#print(selectedPart)
+#-------------------------
+
+#-------------------------
+#message = "Hello, World!"
+
+#print(message)
+
+#selectedPart = message[: 5];
+
+#print(selectedPart)
+#-------------------------
+
+#-------------------------
+#message = "Hello, World!"
+
+#print(message)
+
+#selectedPart = message[3:];
+
+#print(selectedPart)
+#-------------------------
+
+#-------------------------
+#message = "Hello, World!"
+
+#print(message)
+
+#selectedPart = message[-5: -3];
+
+#print(selectedPart)
+#-------------------------
+
+#-------------------------
+#message = "Hello, World!"
+
+#print(message)
+
+#selectedPart = message[-3: -5];
+
+#print(selectedPart)
+#-------------------------
+
+#-------------------------
+#message = "Hello, World!"
+
+#print(message)
+
+#selectedPart = message[-3:];
+
+#print(selectedPart)
+#-------------------------
+
+#-------------------------
+#message = "Hello, World!"
+
+#print(message)
+
+#selectedPart = message[: -5];
+
+#print(selectedPart)
+#-------------------------
+#-------------------------
+#-------------------------
+
+#-------------------------
+#message = "Hello, World!"
+
+#print(message)
+
+#message = message.upper()
+
+#print(message)
+#-------------------------
+
+#-------------------------
+#message = "Hello, World!"
+
+#print(message)
+
+#message = message.lower()
+
+#print(message)
+#-------------------------
+
+#-------------------------
+#message = "   Hello, World!    "
+
+#print(message)
+
+#message = message.strip()
+
+#print(message)
+#-------------------------
+
+#-------------------------
+#message = "   Hello, World!    "
+
+#print(message)
+
+#message = message.rstrip()
+
+#print(message)
+#-------------------------
+
+#-------------------------
+#message = "   Hello, World!    "
+
+#print(message)
+
+#message = message.lstrip()
+
+#print(message)
+#-------------------------
+#-------------------------
+#-------------------------
+
+#-------------------------
+#-------------------------
+#-------------------------
+#message = "   Hello, World!    "
+
+#print(message)
+
+#message = message.replace(" ", "-")
+
+#print(message)
+#-------------------------
+
+#-------------------------
+#message = "   Hello, World!    "
+
+#print(message)
+
+#message = message.strip()
+
+#print(message)
+
+#message = message.replace(" ", "-")
+
+#print(message)
+#-------------------------
+
+#-------------------------
+#message = "   Hello, World!    "
+
+#print(message)
+
+#message = message.rstrip()
+
+#print(message)
+
+#message = message.replace(" ", "-")
+
+#print(message)
+#-------------------------
+
+#-------------------------
+#message = "   Hello, World!    "
+
+#print(message)
+
+#message = message.lstrip()
+
+#print(message)
+
+#message = message.replace(" ", "-")
+
+#print(message)
+#-------------------------
+
+#-------------------------
+#message = "Hello, World!"
+
+#print(message)
+
+#messageParts = message.split(',')
+
+#print(messageParts)
+#-------------------------
