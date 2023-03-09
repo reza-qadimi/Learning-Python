f5836e04fde9fb6f7257abe6b815d38bb0c0444c
reza-qadimi <rezaqadimi.ir@gmail.com>
2023-03-09 23:13:26 +0330

    feature: boolean

--- src/Learning_02/DataTypes/boolean.py
+++ src/Learning_02/DataTypes/boolean.py
@@ -1,1 +1,90 @@
+#-------------------------
+#-------------------------
+#-------------------------
 
+#Booleans represent one of two value: True or False
+
+#Any number is True, except 0.
+
+#Any string is True, except empty strings.
+
+#Any list, tuple, set, and dictionary are True, except empty ones.
+
+#Almost any value is evaluated to True if it has some sort of content.
+
+#In fact, there are not many values that evaluate to False
+#except the value False and empty values, such as (), [], {}, "", the number 0, and the value None.
+
+#-------------------------
+#-------------------------
+#-------------------------
+
+#-------------------------
+#-------------------------
+#-------------------------
+
+#value = 10 > 1 #TRUE
+
+#print(value)
+
+#-------------------------
+
+#value = 1 < 10 #TRUE
+
+#print(value)
+
+#-------------------------
+
+#value = 10 < 1 #FALSE
+
+#print(value)
+
+#-------------------------
+
+#value = 1 > 10 #FALSE
+
+#print(value)
+
+#-------------------------
+
+#value = 11 >= 11 #TRUE
+
+#print(value)
+
+#-------------------------
+
+#value = 11 <= 11 #TRUE
+
+#print(value)
+
+#-------------------------
+
+#value = 11 == 11 #TRUE
+
+#print(value)
+
+#-------------------------
+
+#value = 11 != 11 #FALSE
+
+#print(value)
+
+#-------------------------
+
+#-------------------------
+
+#value = 11 != 12 #TRUE
+
+#print(value)
+
+#-------------------------
+
+#-------------------------
+
+#value = 11
+
+#isInteger = isinstance(value,int) #TRUE
+
+#print(isInteger)
+
+#-------------------------
