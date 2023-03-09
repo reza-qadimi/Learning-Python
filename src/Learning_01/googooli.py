be957f3a64e562c8fafe73b24c7e7d14721d9843
reza-qadimi <rezaqadimi.ir@gmail.com>
2023-03-09 21:24:55 +0330

    make some changes.

---
+++ src/Learning_01/googooli.py
@@ -0,0 +1,4 @@
+# right click on googooli.py
+# Set as Startup file
+# Start
+print('Hello, World!');
--- src/Learning_02/program.py
+++ src/Learning_02/program.py
@@ -1,1 +1,235 @@
+# -------------------------
+# Data-Types
+# -------------------------
 
+# - Built-in Data Types:
+
+# 	Text Type: str
+# 	Numeric Types: int, float, complex
+# 	Sequence Types: list, tuple, range
+# 	Mapping Type: dict
+# 	Set Types: set, frozenset
+# 	Boolean Type: bool
+# 	Binary Types: bytes, bytearray, memoryview
+# 	NoneType: NoneType
+
+# -------------------------
+
+# -------------------------
+
+#value = 5
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = 5.1
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = 5.1j
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = "Hello, World (1)!"
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = 'Hello, World (1)!'
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = [1, 2, 3, 4, 5]
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = [1, '2', 3, "Four", 5]
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = (1, 2, 3, 4, 5)
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = (1, '2', 3, "Four", 5)
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = range(6)
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = range(2, 6)
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = {"value1": 1, "value2": "2", "value3": 3, "Value4": "Four", 5: 5}
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = {"value1": 1, "value2": "2", "value3": 3, "Value4": "Four", 5: 5, 5: "Five", 1: True}
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+# XX Does Not Work with Repetitieve Values XX
+# XX Does Not Work with lists (unhashable type: 'list')XX
+
+#value = {1,'2', 3, "Four", 5, 5, 2, True }
+
+#print(value)
+
+#value = {4,'3', 2, "Four", 3, 6, 80.1, False }
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = frozenset([1, "2", 3, "Four", 5, False])
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = True
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = False
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = b"Hello, World!"
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = bytearray(5)
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = memoryview(bytes(5))
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
+
+#value = None
+
+#print(value)
+
+#typeOfValue = type(value)
+
+#print(typeOfValue)
+
+# -------------------------
