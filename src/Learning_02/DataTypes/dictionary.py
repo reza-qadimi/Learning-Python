0a6913471b32346b84461e5124c17e8248180345
reza-qadimi <rezaqadimi.ir@gmail.com>
2023-03-10 19:21:40 +0330

    dictionary.

--- src/Learning_02/DataTypes/dictionary.py
+++ src/Learning_02/DataTypes/dictionary.py
@@ -233,6 +233,304 @@
 #-------------------------
 #-------------------------
 
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#user["birthDate"] = 1994
+#user["isActive"] = False
+#user["isVerified"] = False
+
+#print(user)
+
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#user["gender"] = "Male"
+
+#print(user)
+
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#user.update({ "isActive": False, "isVerified": False })
+
+#print
+
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#user.update({ "gender": "Male" })
+
+#print(user)
+
+#-------------------------
+#-------------------------
+#-------------------------
+
+#-------------------------
+#-------------------------
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#user.pop("username")
+
+#print(user)
+
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#user.pop() #Runtime Error: pop expected at least 1 argument, got 0
+
+#print(user)
+
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#user.popitem() #"isVerified": True,
+
+#print(user)
+
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#del user["isActive"]
+#del user["isVerified"]
+
+#print(user)
+
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#del user
+
+#print(user) #Runtime Error: name 'user' is not defined
+
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#user.clear()
+
+#print(user)
+
+#-------------------------
+#-------------------------
+#-------------------------
+
+#-------------------------
+#-------------------------
+#-------------------------
+
+#user1 = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user1)
+
+#user2 = user1.copy()
+
+#print(user2)
+
+#-------------------------
+
+#user1 = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user1)
+
+#user2 = dict(user1)
+
+#print(user2)
+
+#-------------------------
+#-------------------------
+#-------------------------
+
+#-------------------------
+#-------------------------
+#-------------------------
+
+#user1 = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#user2 = {
+#	"username": "Farbod-Javan",
+
+#	"firstName": "Farbod",
+#	"lastName": "Fazaeli Javan",
+
+#	"birthDate": 1990,
+
+#	"isActive": False,
+#	"isVerified": True,
+#}
+
+#team2 = {
+#	"backendDeveloper": user1,
+#	"ProductOwner": user2
+#}
+
+#print(team2)
+
+#backendDeveloperFullName =\
+#	team2["backendDeveloper"]["firstName"] +\
+#	" " +\
+#	team2["backendDeveloper"]["lastName"]
+
+#print(backendDeveloperFullName)
 
 #-------------------------
 
