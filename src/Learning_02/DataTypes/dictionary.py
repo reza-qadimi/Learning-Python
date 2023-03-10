8de96af947850e395c51d0482705c7b5bcbef5e9
reza-qadimi <rezaqadimi.ir@gmail.com>
2023-03-10 19:06:28 +0330

    feature: dictionary

---
+++ src/Learning_02/DataTypes/dictionary.py
@@ -0,0 +1,241 @@
+#Dictionaries are used to store data values in key:value pairs.
+#Dictionary items can be referred to by using the key name.
+#A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+
+#Ordered or Unordered?
+#When we say that dictionaries are ordered, it means that the items have a defined order, and that order will not change.
+#Unordered means that the items does not have a defined order, you cannot refer to an item by using an index.
+
+#Changeable
+#Dictionaries are changeable, meaning that we can change, add or remove items after the dictionary has been created.
+
+#Duplicates Not Allowed
+#Dictionaries cannot have two items with the same key:
+
+#-------------------------
+#-------------------------
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#valueDataType = type(user)
+
+#print(valueDataType)
+
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#print(user["username"])
+
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi (1)",
+#	"username": "Reza-Qadimi (2)",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#print(user["username"])
+
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi (1)",
+#	"username": "Reza-Qadimi (2)",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#dictionaryLength = len(user)
+
+#print(dictionaryLength)
+
+#-------------------------
+
+#user = dict(username = "Reza-Qadimi",
+#	firstName = "Reza", lastName = "Qadimi",
+#	birthDate = 1995, isActive =  True, isVerified = True)
+
+#print(user)
+
+#-------------------------
+#-------------------------
+#-------------------------
+
+#-------------------------
+#-------------------------
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#firstName = user["firstName"]
+
+#print(firstName)
+
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#firstName = user.get("firstName")
+
+#print(firstName)
+
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#keys = user.keys()
+
+#print(keys)
+
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#values = user.values()
+
+#print(values)
+
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#items = user.items()
+
+#print(items)
+
+#-------------------------
+
+#user = {
+#	"username": "Reza-Qadimi",
+
+#	"firstName": "Reza",
+#	"lastName": "Qadimi",
+
+#	"birthDate": 1995,
+
+#	"isActive": True,
+#	"isVerified": True,
+#}
+
+#print(user)
+
+#if "firstName" in user:
+#	print(True)
+
+#-------------------------
+#-------------------------
+#-------------------------
+
+#-------------------------
+#-------------------------
+#-------------------------
+
+
+#-------------------------
+
+#-------------------------
+#-------------------------
+#-------------------------
