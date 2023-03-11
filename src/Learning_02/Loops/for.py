99be771d3e0c9bbe55c9e9fdf4dc7bd709f3af89
reza-qadimi <rezaqadimi.ir@gmail.com>
2023-03-11 21:50:22 +0330

    loops.

---
+++ src/Learning_02/Loops/for.py
@@ -0,0 +1,58 @@
+﻿# -------------------------
+
+#numbers = [10, 20, 3, 15]
+
+#for item in numbers:
+#	print(item)
+
+# -------------------------
+
+#numbers = [10, 20, 3, 15]
+
+#for index, item in enumerate(numbers):
+#	message = f"{index + 1}. {item}"
+#	print(message)
+
+# -------------------------
+
+#for item in range(5):
+#	print(item)
+
+# -------------------------
+
+#for item in range(1, 5):
+#	print(item)
+
+# -------------------------
+
+#numbers = [10, 20, 3, 15]
+
+#randomNames = ["ژیان", "ژاپن", "براوو"]
+
+#for number in numbers:
+
+#	for name in randomNames:
+
+#		if number == 10:
+#			message = f"break - {number}"
+#			print(message)
+#			break
+
+#		if number == 3:
+#			message = f"continue - {number}"
+#			print(message)
+
+#			continue
+
+#		message = f"{name} {number}"
+
+#		print(message)
+
+		
+#	if number == 15:
+#		message = f"break - {number}"
+#		print(message)
+#		break
+
+
+# -------------------------
---
+++ src/Learning_02/Loops/while.py
@@ -0,0 +1,34 @@
+# -------------------------
+
+#index = 1
+
+#while index <= 5:
+#	print(index)
+#	index += 1
+
+# -------------------------
+
+#index = 1
+
+#while index <= 5:
+#	print(index)
+
+#	if index == 2:
+#		break
+
+#	index += 1
+
+# -------------------------
+
+#index = 0
+
+#while index <= 5:
+
+#	index += 1
+
+#	if index % 2 == 0:
+#		continue
+
+#	print(index)
+
+# -------------------------
