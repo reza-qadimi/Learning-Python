141d67e888e9150fb85e586527ef78794b5fb49c
reza-qadimi <rezaqadimi.ir@gmail.com>
2023-03-11 21:22:15 +0330

    conditions.

---
+++ src/Learning_02/Conditions/condition.py
@@ -0,0 +1,72 @@
+# -------------------------
+# -------------------------
+# -------------------------
+
+#Equals: a == b
+#Not Equals: a != b
+#Less than: a < b
+#Less than or equal to: a <= b
+#Greater than: a > b
+#Greater than or equal to: a >= b
+
+# -------------------------
+
+#firstNumber = 20
+#secondNumber = 30
+
+#if firstNumber > secondNumber:
+#	print(firstNumber)
+
+#if secondNumber > firstNumber:
+#	print(secondNumber)
+
+# -------------------------
+
+#firstNumber = 20
+#secondNumber = 30
+
+#if firstNumber > secondNumber:
+#	print(firstNumber)
+
+#elif secondNumber > firstNumber:
+#	print(secondNumber)
+
+# -------------------------
+
+#firstNumber = 20
+#secondNumber = 20
+
+#if firstNumber > secondNumber:
+#	print(firstNumber)
+
+#elif secondNumber > firstNumber:
+#	print(secondNumber)
+
+#else:
+#	message = "first number is equal to second number"
+#	print(message)
+
+# -------------------------
+
+#firstNumber = 20
+#secondNumber = 30
+
+#if not firstNumber > secondNumber:
+#	print(secondNumber)
+
+# -------------------------
+
+#The pass Statement
+#if statements cannot be empty, but if you for some reason have an if statement with no content, put in the pass statement to avoid getting an error.
+
+#firstNumber = 20
+#secondNumber = 30
+
+#if firstNumber > secondNumber:
+#	pass
+#else:
+#	print("I do not know what to say!")
+
+# -------------------------
+# -------------------------
+# -------------------------
