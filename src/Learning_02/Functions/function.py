397c1886651b0e5cd9310b3ac2237b8e521e866c
reza-qadimi <rezaqadimi.ir@gmail.com>
2023-03-11 22:26:40 +0330

    feature: function.

---
+++ src/Learning_02/Functions/function.py
@@ -0,0 +1,93 @@
+# -------------------------
+# -------------------------
+# -------------------------
+
+#A function is a block of code which only runs when it is called.
+
+#You can pass data, known as parameters, into a function.
+
+#A function can return data as a result.
+
+# -------------------------
+# -------------------------
+# -------------------------
+
+#def print_text():
+#	print("text")
+
+# -------------------------
+
+#def print_text():
+#	print("text")
+
+#print_text()
+
+# -------------------------
+
+#def print_text(text):
+#	print(text)
+
+#print_text("text")
+
+# -------------------------
+
+#def print_text(text):
+#	print(text)
+
+#print_text("text")
+
+# -------------------------
+
+#def printSumOfTwoNumber(firstNumber, secondNumber):
+#	result = firstNumber + secondNumber
+#	print(result)
+
+#printSumOfTwoNumber(firstNumber = 1, secondNumber = 2)
+
+# -------------------------
+
+#def printSumOfTwoNumber(firstNumber, secondNumber):
+#	result = firstNumber + secondNumber
+#	print(result)
+
+#printSumOfTwoNumber(secondNumber = 1, firstNumber = 2)
+
+# -------------------------
+
+#def get_my_favorite_car(*cars):
+#	favoriteCar = cars[0]
+
+#	return favoriteCar
+
+#result = get_my_favorite_car("Benz", "BMW", "Prosche")
+
+#print(result)
+
+# -------------------------
+
+#def get_user_age(**user):
+#	age = user["age"]
+
+#	return age
+
+#result = get_user_age(username = "RezaQadimi", age = 27)
+
+#print(result)
+
+# -------------------------
+
+#def print_text(text = "Reza (1)"):
+#	print(text)
+
+#print_text()
+
+#print_text("Reza (2)")
+
+# -------------------------
+
+#def usingPassForFunction():
+#	pass
+
+# -------------------------
+# -------------------------
+# -------------------------
