e7af9e15b26427dd78d467f8c75ae55cfa582149
reza-qadimi <rezaqadimi.ir@gmail.com>
2023-03-12 21:37:44 +0330

    add lambda.

---
+++ src/Learning_02/Functions/lambda.py
@@ -0,0 +1,50 @@
+# -------------------------
+# -------------------------
+# -------------------------
+
+#A lambda function is a small anonymous function.
+
+#A lambda function can take any number of arguments, but can only have one expression.
+
+# -------------------------
+# -------------------------
+# -------------------------
+
+# -------------------------
+# -------------------------
+# -------------------------
+
+#lambda arguments : expression
+
+# -------------------------
+
+#getSum = lambda value : value + 10
+
+#result = getSum(value = 12)
+
+#print (result)
+
+# -------------------------
+
+#getSum = lambda firstValue, secondValue : firstValue * secondValue
+
+#result = getSum(firstValue = 5, secondValue = 7)
+
+#print(result)
+
+# -------------------------
+
+#def sumWithValue(inputValue):
+#	result = lambda value : inputValue + value
+
+#	return result
+
+#getSum = sumWithValue(inputValue = 15)
+
+#result = getSum(value = 12)
+
+#print(result)
+
+# -------------------------
+# -------------------------
+# -------------------------
