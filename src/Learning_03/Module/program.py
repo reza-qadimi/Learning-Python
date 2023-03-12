d20e0a874f0c254f522ab3ffa762717ff9a8dd0a
reza-qadimi <rezaqadimi.ir@gmail.com>
2023-03-12 23:02:59 +0330

    update.

--- src/Learning_03/Module/program.py
+++ src/Learning_03/Module/program.py
@@ -40,11 +40,11 @@
 ##Using the dir() Function
 ##There is a built-in function to list all the function names (or variable names) in a module. The dir() function:
 
-import platform;
+#import platform;
 
-platformNames = dir(platform)
+#platformNames = dir(platform)
 
-print(platformNames)
+#print(platformNames)
 
 # -------------------------
 # -------------------------
