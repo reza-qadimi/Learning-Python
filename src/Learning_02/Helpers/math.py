2f490b1695c9d5578e85296fc381c21666ba11f0
reza-qadimi <rezaqadimi.ir@gmail.com>
2023-03-14 22:54:34 +0330

    make some changes.

---
+++ src/Learning_02/Helpers/math.py
@@ -0,0 +1,66 @@
+#-------------------------
+#-------------------------
+#-------------------------
+
+#numbers = [10, 20, 3, 15]
+
+#maxValue = max(numbers)
+
+#print("Max Value:", maxValue)
+
+#minValue = min(numbers)
+
+#print("Min Value:", minValue)
+
+#-------------------------
+
+##The abs() function returns the absolute (positive) value of the specified number:
+
+#value = abs(-10.23)
+
+#print(value)
+
+#-------------------------
+
+##The pow(x, y) function returns the value of x to the power of y (xy)
+
+#value = pow(4, 2)
+
+#print(value)
+
+#-------------------------
+
+##The math.sqrt() method for example, returns the square root of a number:
+
+#import math;
+
+#value = math.sqrt(128)
+
+#print(value)
+
+#-------------------------
+
+#import math;
+
+#value = math.pi
+
+#print(value)
+
+#-------------------------
+
+##https://www.w3schools.com/python/module_math.asp
+
+#import math;
+
+#ceilOfValue = math.ceil(1.4)
+
+#print("Ceil Of Value:", ceilOfValue)
+
+#floorOfValue = math.floor(1.4)
+
+#print("Floor Of Value:", floorOfValue)
+
+#-------------------------
+#-------------------------
+#-------------------------
+
