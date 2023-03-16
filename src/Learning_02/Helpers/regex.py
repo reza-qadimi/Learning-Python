6f0da184b0ba7c77058899b37b87fedcfb037dce
reza-qadimi <rezaqadimi.ir@gmail.com>
2023-03-16 13:35:33 +0330

    regex

---
+++ src/Learning_02/Helpers/regex.py
@@ -0,0 +1,32 @@
+#-------------------------
+#-------------------------
+#-------------------------
+
+#A RegEx, or Regular Expression, is a sequence of characters that forms a search pattern.
+
+#RegEx can be used to check if a string contains the specified search pattern.
+
+#import re;
+
+#text = "Hello, World!"
+
+#result = re.search("^Hello", text)
+
+#if result:
+#	print("Hi...")
+
+#else:
+#	print("Hello :)")
+
+#-------------------------
+
+#https://www.w3schools.com/python/python_regex.asp
+
+#findall:	Returns a list containing all matches
+#sub:		Replaces one or many matches with a string
+#split:		Returns a list where the string has been split at each match
+#search:	Returns a Match object if there is a match anywhere in the string
+
+#-------------------------
+#-------------------------
+#-------------------------
