9e1eb7e73a36fed9a023ff36e108f5106c894e04
reza-qadimi <rezaqadimi.ir@gmail.com>
2023-03-16 15:01:57 +0330

    read file.

---
+++ src/Learning_04/FileManager/openfile.py
@@ -0,0 +1,84 @@
+# -------------------------
+# -------------------------
+# -------------------------
+
+#"r" - Read - Default value. Opens a file for reading, error if the file does not exist
+
+#"a" - Append - Opens a file for appending, creates the file if it does not exist
+
+#"w" - Write - Opens a file for writing, creates the file if it does not exist
+
+#"x" - Create - Creates the specified file, returns an error if the file exists
+
+# -------------------------
+
+#"t" - Text - Default value. Text mode
+
+#"b" - Binary - Binary mode (e.g. images)
+
+# -------------------------
+# -------------------------
+# -------------------------
+
+# -------------------------
+# -------------------------
+# -------------------------
+
+#filePath = "FileManager\\openfile.py"
+##filePath = "..\\Learning_02\\_documents\\Readme.txt"
+
+#foundedFile = open(filePath)
+
+#for line in foundedFile:
+#	print(line)
+
+# -------------------------
+
+##"r" - Read - Default value. Opens a file for reading, error if the file does not exist
+
+##"a" - Append - Opens a file for appending, creates the file if it does not exist
+
+##"w" - Write - Opens a file for writing, creates the file if it does not exist
+
+##"x" - Create - Creates the specified file, returns an error if the file exists
+
+#filePath = "FileManager\\openfile.py"
+
+#foundedFile = open(file = filePath, mode = "r")
+
+#for line in foundedFile:
+#	print(line)
+
+# -------------------------
+
+##"t" - Text - Default value. Text mode
+
+##"b" - Binary - Binary mode (e.g. images)
+
+#filePath = "FileManager\\openfile.py"
+
+#foundedFile = open(file = filePath, mode = "rt")
+
+#for line in foundedFile:
+#	print(line)
+
+# -------------------------
+
+#filePath = "FileManager\\openfile.py"
+
+#foundedFile = open(file = filePath, mode = "rt")
+
+#print(foundedFile.read())
+
+# -------------------------
+
+#filePath = "FileManager\\openfile.py"
+
+#foundedFile = open(file = filePath, mode = "rt")
+
+##you can also specify how many characters you want to return:
+#print(foundedFile.read(150))
+
+# -------------------------
+# -------------------------
+# -------------------------
