0dc4b91b90ff42e92e548986ed7b7c6cebafd7ef
reza-qadimi <rezaqadimi.ir@gmail.com>
2023-03-17 19:51:43 +0330

    binomial

--- src/NumbPy/Step_01/Learning_03.py
+++ src/NumbPy/Step_01/Learning_03.py
@@ -77,3 +77,46 @@
 #--------------------------------------------------
 #--------------------------------------------------
 #--------------------------------------------------
+#Binomial Distribution:
+#--------------------------------------------------
+
+#Binomial Distribution is a Discrete Distribution.
+
+#It describes the outcome of binary scenarios, e.g. toss of a coin, it will either be head or tails.
+
+#It has three parameters:
+
+#n - number of trials.
+
+#p - probability of occurence of each trial (e.g. for toss of a coin 0.5 each).
+
+#size - The shape of the returned array.
+
+#Difference Between Normal and Binomial Distribution:
+#The main difference is that normal distribution is continous whereas binomial is discrete, but if there are enough data points it will be quite similar to normal distribution with certain loc and scale.
+
+#--------------------------------------------------
+
+#from numpy import random
+
+#result = random.binomial(n=10, p=0.5, size=10)
+
+#print(result)
+
+#--------------------------------------------------
+
+#from numpy import random
+
+#import seaborn
+#import matplotlib.pyplot as pyplot
+
+#result = random.binomial(n=10, p=0.5, size=1000)
+
+#seaborn.histplot(result)
+##seaborn.distplot(result, hist=True, kde=False)
+
+#pyplot.show()
+
+#--------------------------------------------------
+#--------------------------------------------------
+#--------------------------------------------------
